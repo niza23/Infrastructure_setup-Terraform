86d3da93eb42d9d0fe53d11ac2620f957f194853
Nidhi Zala <nidhizala2307@gmail.com>
2025-08-11 00:41:24 +0530

    Add files via upload

---
+++ varioables.tf
@@ -0,0 +1,55 @@
+# AWS basic variables
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  description = "AWS CLI profile to use"
+  type        = string
+  default     = "default"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to use"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+  default     = "ami-0261755bbcb8c4a84"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+  default     = "terraform-demo-bucket-2025"
+}
+
+#env variable
+variable "environment" {
+  description = "Environment type (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
