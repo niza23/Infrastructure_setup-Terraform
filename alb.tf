dedcadf4a2c0272755e7e865cd94a29ac5385313
Nidhi Zala <nidhizala2307@gmail.com>
2025-08-11 00:42:10 +0530

    Add files via upload

---
+++ alb.tf
@@ -0,0 +1,48 @@
+resource "aws_lb" "alb" {
+  name               = "terraform-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web_sg.id]
+  subnets            = aws_subnet.public[*].id
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = merge(local.common_tags, { Name = "Terraform-ALB" })
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "terraform-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+
+  tags = merge(local.common_tags, { Name = "Target-Group" })
+}
+
+resource "aws_lb_target_group_attachment" "attach_web1" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach_web2" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.tg.arn
+    type             = "forward"
+  }
+}
